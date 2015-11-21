00e18f96d74c74e198b5446e048f0db1dd8c38f6
Daenks <daenks@daenks.org>
2015-11-21 13:44:10 -0600

    removed base class constructor code, handed by new subclasses

--- ClientPatcher/PatcherSettings/PatcherSettings.cs
+++ ClientPatcher/PatcherSettings/PatcherSettings.cs
@@ -20,15 +20,7 @@ namespace ClientPatcher
 
         public PatcherSettings()
         {
-            ServerName = "103";
-            PatchInfoUrl = "http://ww1.openmeridian.org/103/patchinfo.txt";
-            FullInstallUrl = "http://ww1.openmeridian.org/103/Meridian59.Client.Classic.zip";
-            ClientFolder = "C:\\Program Files\\Open Meridian\\Meridian 103";
-            PatchBaseUrl = "http://ww1.openmeridian.org/103/clientpatch";
-            AccountCreationUrl = "http://ww1.openmeridian.org/103/acctcreate.php";
-            Guid = "5AD1FB01-A84A-47D1-85B8-5F85FB0C201E";
-            Default = true;
-            ClientType = ClientType.Classic;
+
         }
 
         public PatcherSettings(string servername, string patchinfourl, string clientfolder, string patchbaseurl, string fullinstallurl, bool defaultserver = false, ClientType clientType = ClientType.Classic)
