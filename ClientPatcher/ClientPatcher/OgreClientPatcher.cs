780d5cf8426b9cbd702773840b902d6af3217a57
Matthew Trey <m.d.trey@gmail.com>
2015-11-23 16:02:33 -0500

    Move client launch code into their appropriate ClientPatcher children

--- ClientPatcher/ClientPatcher/OgreClientPatcher.cs
+++ ClientPatcher/ClientPatcher/OgreClientPatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Diagnostics;
 using PatchListGenerator;
 
 namespace ClientPatcher
@@ -27,6 +28,19 @@ namespace ClientPatcher
             {
                 sw.Write(scanner.ToJson());
             }    
+        }
+        public override void Launch()
+        {
+            var meridian = new ProcessStartInfo
+            {
+                FileName = CurrentProfile.ClientFolder + "\\x86\\Meridian59.Ogre.Client.exe",
+                WorkingDirectory = CurrentProfile.ClientFolder + "\\x86\\",
+
+                //TODO: add ability to enter username and password during patching
+                //meridian.Arguments = "/U:username /P:password /H:host";
+            };
+
+            Process.Start(meridian);
         }
     }
 }
