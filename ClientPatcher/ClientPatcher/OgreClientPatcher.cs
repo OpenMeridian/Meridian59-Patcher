95e111777a7841a48f9122428e30f3fab2100e4f
skittles1 <tmx883@gmail.com>
2015-12-03 01:01:55 +0800

    Add VS 2013 C++ redist. dependency check.

--- ClientPatcher/ClientPatcher/OgreClientPatcher.cs
+++ ClientPatcher/ClientPatcher/OgreClientPatcher.cs
@@ -9,8 +9,13 @@ namespace ClientPatcher
     class OgreClientPatcher : ClientPatcher
     {
         #region Constructors
-        private string NETDownloadUri = "https://www.microsoft.com/en-US/download/details.aspx?id=48130";
-        private string NETDownloadMsg = "The Ogre client requires .NET version 4.5.1 or greater, please install this to continue.";
+        private const string NETRegistryKey = "SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\";
+        private const string NETDownloadUri = "https://www.microsoft.com/en-US/download/details.aspx?id=48130";
+        private const string NETDownloadMsg = "The Ogre client requires .NET version 4.5.1 or greater, please install this to continue.";
+        private const string VSRedistx86Key = "SOFTWARE\\Wow6432Node\\Microsoft\\VisualStudio\\12.0\\VC\\Runtimes\\x86";
+        private const string VSRedistx64Key = "SOFTWARE\\Wow6432Node\\Microsoft\\VisualStudio\\12.0\\VC\\Runtimes\\x64";
+        private const string VSRedistDlUri = "https://www.microsoft.com/en-US/download/details.aspx?id=40784";
+        private const string VSRedistDlMsg = "The Ogre client requires the VS 2013 C++ redistributable, please install this to continue.";
 
         public OgreClientPatcher(PatcherSettings settings)
             : base(settings)
@@ -57,7 +62,7 @@ namespace ClientPatcher
         public override bool CheckDependencies()
         {
             using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,
-                RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\"))
+                RegistryView.Registry32).OpenSubKey(NETRegistryKey))
             {
                 int releaseKey = Convert.ToInt32(ndpKey.GetValue("Release"));
                 // If a user has a .NET version lower than 4.5, send them to download it.
@@ -68,9 +73,29 @@ namespace ClientPatcher
 
                     return false;
                 }
-
-                return true;
             }
+
+            string VSRedistKey;
+            if (Environment.Is64BitOperatingSystem)
+                VSRedistKey = VSRedistx64Key;
+            else
+                VSRedistKey = VSRedistx86Key;
+
+            using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine,
+                RegistryView.Registry32).OpenSubKey(VSRedistKey))
+            {
+                // null key converts to 0.
+                int releaseKey = Convert.ToInt32(ndpKey.GetValue("Installed"));
+                // If a user does not have VS 2013 C++ redist installed, download them.
+                if (releaseKey == 0)
+                {
+                    OnFailedDependency(new FailedDependencyEventArgs(VSRedistDlUri, VSRedistDlMsg));
+
+                    return false;
+                }
+            }
+
+            return true;
         }
         #endregion
     }
